7d10c40ea2ced00b1009d436bd03ddf8c88c362e
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2020-05-24 13:38:24 +0200

    añadido index.html y primera app.py en flask

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+app = Flask(__name__)
 
-print('hola') 
-print ('hola2')
+@app.route('/')
+def hello_world():
+	suma = 100+200
+	return render_template('index.html', suma=suma)
+
+@app.route('/raul')
+def raul():
+    return '<h1>Hola Raul!</h1> <p> que pasa raul </p>'
